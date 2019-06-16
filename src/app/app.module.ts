f0d3f212e1e0b13a85f4ebd46405b4926c230275
samuelacpl <samuela.cappelluti@gmail.com>
2019-06-16 19:20:03 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TabelloneComponent } from './tabellone/tabellone.component';
+import { MatchComponent } from './match/match.component';
+import { SquadComponent } from './squad/squad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabelloneComponent
+    TabelloneComponent,
+    MatchComponent,
+    SquadComponent
   ],
   imports: [
     BrowserModule,
